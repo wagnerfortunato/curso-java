fe9505147c9be5277da76a9a729c911c7ed8c541
wagnerfortunato <wagnerjonnatasjc@gmail.com>
2024-12-16 23:14:14 -0300

    Primeiro commit

---
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,36 @@
+package fundamentos;
+
+/**
+ *  Essa Classe representa... JAVADOC
+ *  
+ * @author Wagner
+ * @since JDK1.0
+ * @see
+ */
+
+public class PrimeiroPrograma {
+	
+	/**
+	 * ...
+	 * @param args
+	 * 
+	 */
+	
+	public static void main(String[] args) {	
+		
+		//Uma sentença de código termina com ;
+		//Mais um comentário...
+		//Fim
+		System.out.println("Primeiro programa Parte #1!");
+		
+		/*
+		 Linha 1
+		 Linha 2
+		 Linha 3
+		 */
+		
+		System.out.println("Primeiro programa Parte #2!");
+		System.out.println("Fim");// Aqui tambem funciona!
+	}
+
+}
